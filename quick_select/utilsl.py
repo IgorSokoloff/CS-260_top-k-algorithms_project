0528d3c040ef0449083dd0e67a5c3553c9c4bd9a
Igor Sokolov <igor.a.sokolov@phystech.edu>
2021-11-21 14:30:10 +0300

    Added some tests of tournament method

--- quick_select/utilsl.py
+++ quick_select/utilsl.py
@@ -1,5 +1,10 @@
 from random import randint
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+sys.setrecursionlimit(15000)
 
+N_COMPARISONS_QS__ = 0
 
 def swap(arr, i, j):
     arr[i], arr[j] = arr[j], arr[i]
@@ -15,6 +20,7 @@ def partition(arr, left, right, p, key):
     swap(arr, p, right)
     p = left
     for i in range(left, right):
+        quickselect.N_COMPARISONS_QS += 1
         if key(arr[i]) <= key(pivot):
             swap(arr, i, p)
             p = p + 1
@@ -24,6 +30,7 @@ def partition(arr, left, right, p, key):
 
 def quickselect(arr, k, left=None, right=None, key=lambda x: x):
     if left is None or right is None:
+        quickselect.N_COMPARISONS_QS = 0
         arr = arr.copy()
         left = 0
         right = len(arr) - 1
@@ -31,6 +38,7 @@ def quickselect(arr, k, left=None, right=None, key=lambda x: x):
         return arr[left]
     p = randint(left, right)
     p = partition(arr, left, right, p, key)
+    quickselect.N_COMPARISONS_QS += 1
     if k == p:
         return arr[k]
     elif k < p:
@@ -50,6 +58,7 @@ def topk(arr, k, key=lambda x: x, inplace=True):
     equals = []
     removed = 0
     for i in range(len(arr)):
+        quickselect.N_COMPARISONS_QS += 1
         if arr[i] < kth:
             removed += 1
             arr[i] = 0
@@ -57,6 +66,7 @@ def topk(arr, k, key=lambda x: x, inplace=True):
             equals.append(i)
     #stupid part
     for i in equals:
+        quickselect.N_COMPARISONS_QS += 1
         if n - removed > k:
             arr[i] = 0
             removed += 1
@@ -76,10 +86,31 @@ def test():
     top = topk(arr, 5, inplace=False)
     assert top == [1, 9, 4, 5, 63]
 
+def test_complexity(type='average', nmax = 5000, nmin=20, step = 1000, average = 10, k=10, output=None):
+    ns = np.arange(nmin,nmax,step)
+    niters = np.zeros(len(ns))
+    iters = 0
+    for i, n in enumerate(ns):
+        if type == 'worst':
+            arr = np.arange(n, dtype=np.int)[::-1]
+        else:
+            arr = np.random.rand(n)
+        for _ in range(average):
+            topk(arr,k)
+            iters += quickselect.N_COMPARISONS_QS
+        iters /= average
+        niters[i] = iters
+    if output is None:
+        plt.plot(ns,niters)
+        plt.show()
 
 
 if __name__ == '__main__':
-    arr = [1, 3, 20, 30, 9, 2, 31, 7, 2]
+    arr = [1, 1, 1, 1, 1, 1, 1, 1, 1] * 1000
+    test_complexity(type='worst')
+    exit(0)
     top = quickselect(arr, 2)
     topk(arr, 7)
+    print(quickselect.N_COMPARISONS_QS)
     test()
+    print(quickselect.N_COMPARISONS_QS)

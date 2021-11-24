066db2822a218163268c36679b33d7f2c5121e3c
Yasir Ghunaim <yasirghunaim@gmail.com>
2021-11-24 21:26:18 +0300

    Added pivotType for all functions and implemented deterministic

--- quick_select/utilsl.py
+++ quick_select/utilsl.py
@@ -2,9 +2,14 @@ from random import randint
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+from enum import Enum
 sys.setrecursionlimit(15000)
 
 N_COMPARISONS_QS__ = 0
+class PivotType(Enum):
+    RANDOM = 1
+    DETERMINISTIC = 2
+    MEDIAN = 3
 
 def swap(arr, i, j):
     arr[i], arr[j] = arr[j], arr[i]
@@ -28,7 +33,7 @@ def partition(arr, left, right, p, key):
     return p
 
 
-def quickselect(arr, k, left=None, right=None, key=lambda x: x):
+def quickselect(arr, k, left=None, right=None, key=lambda x: x, pivotType=PivotType.RANDOM):
     if left is None or right is None:
         quickselect.N_COMPARISONS_QS = 0
         arr = arr.copy()
@@ -36,25 +41,34 @@ def quickselect(arr, k, left=None, right=None, key=lambda x: x):
         right = len(arr) - 1
     if left == right:
         return arr[left]
-    p = randint(left, right)
+
+    # Random Pivot
+    if pivotType == PivotType.RANDOM:
+        p = randint(left, right)
+        
+    # Deterministic Pivot - choose middle index
+    elif pivotType == PivotType.DETERMINISTIC:
+        p = left + (abs(left-right))//2
+
+
     p = partition(arr, left, right, p, key)
     quickselect.N_COMPARISONS_QS += 1
     if k == p:
         return arr[k]
     elif k < p:
-        return quickselect(arr, k, left, p - 1, key)
+        return quickselect(arr, k, left, p - 1, key, pivotType)
     else:
-        return quickselect(arr, k, p + 1, right, key)
+        return quickselect(arr, k, p + 1, right, key, pivotType)
 
 
-def topk(arr, k, key=lambda x: x, inplace=True):
+def topk(arr, k, key=lambda x: x, inplace=True, pivotType = PivotType.RANDOM):
     # key=lambda x: -key(x) we find top, so the order is reversed
     n = len(arr)
     if not inplace:
         arr = arr.copy()
     if k < 1 or k > n:
         raise UserWarning('k Value is outside of 1...N')
-    kth = quickselect(arr, k-1, key=lambda x: -key(x))
+    kth = quickselect(arr, k-1, key=lambda x: -key(x), pivotType=pivotType)
     equals = []
     removed = 0
     for i in range(len(arr)):
@@ -73,20 +87,20 @@ def topk(arr, k, key=lambda x: x, inplace=True):
     if not inplace:
         return arr
 
-def test():
+def test(pivotType=PivotType.RANDOM):
     arr = [1, 9, 4, 5, 63]
-    top = topk(arr, 1, inplace=False)
+    top = topk(arr, 1, inplace=False, pivotType=pivotType)
     assert top == [0, 0, 0, 0, 63]
-    top = topk(arr, 2, inplace=False)
+    top = topk(arr, 2, inplace=False, pivotType=pivotType)
     assert top == [0, 9, 0, 0, 63]
-    top = topk(arr, 3, inplace=False)
+    top = topk(arr, 3, inplace=False, pivotType=pivotType)
     assert top == [0, 9, 0, 5, 63]
-    top = topk(arr, 4, inplace=False)
+    top = topk(arr, 4, inplace=False, pivotType=pivotType)
     assert top == [0, 9, 4, 5, 63]
-    top = topk(arr, 5, inplace=False)
+    top = topk(arr, 5, inplace=False, pivotType=pivotType)
     assert top == [1, 9, 4, 5, 63]
 
-def test_complexity(type='average', nmax = 5000, nmin=20, step = 1000, average = 10, k=10, output=None):
+def test_complexity(type='average', nmax = 5000, nmin=20, step = 1000, average = 10, k=10, output=None, pivotType=PivotType.RANDOM):
     ns = np.arange(nmin,nmax,step)
     niters = np.zeros(len(ns))
     iters = 0
@@ -96,7 +110,7 @@ def test_complexity(type='average', nmax = 5000, nmin=20, step = 1000, average =
         else:
             arr = np.random.rand(n)
         for _ in range(average):
-            topk(arr,k)
+            topk(arr,k,pivotType=pivotType)
             iters += quickselect.N_COMPARISONS_QS
         iters /= average
         niters[i] = iters
@@ -109,8 +123,9 @@ if __name__ == '__main__':
     arr = [1, 1, 1, 1, 1, 1, 1, 1, 1] * 1000
     test_complexity(type='worst')
     exit(0)
-    top = quickselect(arr, 2)
-    topk(arr, 7)
+    pivotType = PivotType.DETERMINISTIC
+    top = quickselect(arr, 2, pivotType=pivotType)
+    topk(arr, 7, pivotType=pivotType)
     print(quickselect.N_COMPARISONS_QS)
-    test()
+    test(pivotType=pivotType)
     print(quickselect.N_COMPARISONS_QS)
